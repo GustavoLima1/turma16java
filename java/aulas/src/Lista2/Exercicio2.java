e905fcaa1617288a9bd3062dc8518b166b141d99
Gustavo Lima <gustavo1.l@hotmail.com>
2021-01-27 15:08:19 -0300

    alteração de decrescente para crescente

--- java/aulas/src/Lista2/Exercicio2.java
+++ java/aulas/src/Lista2/Exercicio2.java
@@ -9,16 +9,16 @@ public class Exercicio2
 		Scanner read = new Scanner(System.in);
 		int n1, n2, n3;
 		
-		System.out.println("Digite o primeiro número");
+		System.out.println("Digite o primeiro número: ");
 		n1 = read.nextInt();
-		System.out.println("Digite o segundo número");
+		System.out.println("Digite o segundo número: ");
 		n2 = read.nextInt();
-		System.out.println("Digite o terceiro número");
+		System.out.println("Digite o terceiro número: ");
 		n3 = read.nextInt();
 		
-		if(n1 > n2 && n1 > n3)
+		if(n1 < n2 && n1 < n3)
 		{
-			if( n2 > n3)
+			if( n2 < n3)
 			{
 				System.out.printf("A ordem crescente é: %d %d %d",n1,n2,n3);
 			}
@@ -27,9 +27,9 @@ public class Exercicio2
 				System.out.printf("A ordem crescente é: %d %d %d",n1,n3,n2);
 			}
 		}
-		else if(n2 > n3)
+		else if(n2 < n3)
 		{
-			if(n1 > n3)
+			if(n1 < n3)
 			{
 				System.out.printf("A ordem crescente é: %d %d %d",n2,n1,n3);
 			}
@@ -40,7 +40,7 @@ public class Exercicio2
 		}
 		else 
 		{
-			if(n1 > 2)
+			if(n1 < n2)
 			{
 				System.out.printf("A ordem crescente é: %d %d %d",n3,n1,n2);
 			}
